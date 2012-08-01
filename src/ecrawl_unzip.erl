27be6c57c4ca87d497deae65f0e99b1b7e2ab510
Hemendra Srivastava <hemendra26@gmail.com>
2012-08-01 22:34:57 +0530

    changes

--- src/ecrawl_unzip.erl
+++ src/ecrawl_unzip.erl
@@ -6,9 +6,9 @@ unzip() ->
     Li = filelib:wildcard("priv/results/*"),
     F = fun(X) ->
 		{ok, Bin} = file:read_file(X),
-		["priv", "results", FName] = re:split(X, "/"),
+		[<<"priv">>, <<"results">>, FName] = re:split(X, "/"),
 		Data = erlang:binary_to_list(zlib:unzip(Bin)),
-		file:write_file("priv/unzipped_data/"++FName++".html", Data)
+		file:write_file("priv/unzipped_data/"++erlang:binary_to_list(FName)++".html", Data)
 	end,
     lists:foreach(F, Li).
 		
